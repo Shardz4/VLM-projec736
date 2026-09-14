c7019eed5e212c79db8cd7da1aaf53b7c92b7e4b
Arnav Sharma <arnav4324@gmail.com>
2026-09-14 23:28:36 +0530

    docs: update evaluators init script

--- src/evaluators/__init__.py
+++ src/evaluators/__init__.py
@@ -1,9 +1,12 @@
 """Evaluation probes and benchmarking utilities for Vision-Language Models."""
 from .counting_evaluator import CountingEvaluator
 from .spatial_evaluator import SpatialEvaluator, make_spatial_prompts
+from .imagenet_evaluator import ImageNetV2Evaluator, load_imagenet_class_names
 
 __all__ = [
     "CountingEvaluator",
     "SpatialEvaluator",
     "make_spatial_prompts",
+    "ImageNetV2Evaluator",
+    "load_imagenet_class_names",
 ]
